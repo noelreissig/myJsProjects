73c5adc968d6588f64925edd05fa7e98b94da402
noelreissig <NREISSIG@GMAIL.COM>
2021-10-12 17:10:34 -0700

    Create friend4letters.js

---
+++ friend4letters.js
@@ -0,0 +1,9 @@
+function friend4letters(friends) {
+  const friend = ["Ryan", "Kieran", "Mark"];
+  friends.forEach(function (name) {
+    if (name.lenght != 4) {
+      friends.filter(name);
+    }
+  });
+  return friends;
+}
